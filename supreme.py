562c16a5f9b3abe5ee16b88cf4985794b63267c7
cbrandt6 <cbrandt6@ycp.edu>
2019-12-03 13:31:20 -0500

    Changed driver file path thing

--- supreme.py
+++ supreme.py
@@ -1,16 +1,17 @@
 from selenium import webdriver
+from driver_file_path import file_path as fp
 from selenium import *
 
-#right click on chromedriver.exe copy the path, and paste it after the r
+# right click on chromedriver.exe copy the path, and paste it after the r
 
-driver = webdriver.Chrome(r"C:\Users\Carl\PycharmProjects\seleniumtest2\drivers\chromedriver.exe")
+driver = webdriver.Chrome(fp)
 
 
 driver.get("https://SupremeNewYork.com/shop")
-#driver.maximize_window()
+# driver.maximize_window()
 
-#opening a broswer and maximizing it in selenium takes approximately 10 years
-#we need to get a workaround
-#going to be copied from stack overflow
-#https://stackoverflow.com/questions/8344776/can-selenium-interact-with-an-existing-browser-session
-#could also just open the browser early
+# opening a broswer and maximizing it in selenium takes approximately 10 years
+# we need to get a workaround
+# going to be copied from stack overflow
+# https://stackoverflow.com/questions/8344776/can-selenium-interact-with-an-existing-browser-session
+# could also just open the browser early
